88e846f9c6f6b7f0b180757a16b0967131837735
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-11 00:35:42 +0530

    Add files via upload

---
+++ Print integer type element in the list.py.py
@@ -0,0 +1,8 @@
+ #Print integer type element in the list.
+def values(my_list):
+    for i in my_list:
+        if (type(i)is int):
+            print(i)
+       
+values(['python',10,'with',15,'datascience','training'])
+    
