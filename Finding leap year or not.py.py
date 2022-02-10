3c2ada36a5efe6bf35d3134065208553b108778f
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-11 00:35:04 +0530

    Add files via upload

---
+++ Finding leap year or not.py.py
@@ -0,0 +1,11 @@
+#Finding leap year or not
+import calendar
+x = calendar.isleap(2019)
+y = calendar.isleap(2022)
+print("2019 is a leap year is",x)
+print("2022 is a leap year is",y)
+import calendar
+x = calendar.isleap(2019)
+y = calendar.isleap(2022)
+print("2019 is a leap year is",x)
+print("2022 is a leap year is",y)
