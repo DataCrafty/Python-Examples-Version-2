995c533c66e80fdcd0bac956a1c388b7dfa864f0
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-11 00:39:02 +0530

    Add files via upload

---
+++ To find the round values by using the map function .py.py
@@ -0,0 +1,8 @@
+#To find the round values by using the map function ?
+def s(x):
+    return x
+value = [12.65,3.42,54.6,75.786,23.44]
+updated_list = map(round,value)
+print(updated_list)
+print(list(updated_list))
+    
