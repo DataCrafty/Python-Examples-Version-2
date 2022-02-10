50be3311a3cda314e98b1f3ca11cc134a5695b64
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-11 01:51:32 +0530

    Add files via upload

---
+++ Write a program to remove the string in the given set of strings.py.py
@@ -0,0 +1,7 @@
+#Write a program to remove the string in the given set of strings
+def s(my,my_list):
+    return my+my_list
+my = {'chandu','shall','vini'}
+my_list = ['kavi','ani','pandu']
+my.remove('chandu')
+print(my)
