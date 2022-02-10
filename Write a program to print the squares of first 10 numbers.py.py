0a535bca90942f88917d7c8d1cd685ab54a1e25c
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-11 01:50:16 +0530

    Add files via upload

---
+++ Write a program to print the squares of first 10 numbers.py.py
@@ -0,0 +1,5 @@
+# to print the squares of first 10 numbers 
+def square(num):
+    for i in range(2,num+1): # squaring the number and to get from second number,if we not give num here it will starts from 0
+        print(i**2)          # it will squares the numbers from starting
+square(10)                   # ten square numbrs 
