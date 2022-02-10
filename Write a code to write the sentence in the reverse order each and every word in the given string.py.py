99ef15b78fbf3babc73f16a6740f38d80c8eb851
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-11 01:11:08 +0530

    Add files via upload

---
+++ Write a code to write the sentence in the reverse order each and every word in the given string.py.py
@@ -0,0 +1,6 @@
+#Write a code to write the sentence in the reverse order each and every word in the given string
+def s(sentence):
+    #words = sentence.split(' ')
+    sentence.reversed()
+    return sentence
+s('chandu naveen pandu')
