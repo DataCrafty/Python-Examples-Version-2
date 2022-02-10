b17fa3bd154a6141884846f705dd3f496c07c257
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-11 01:50:42 +0530

    Add files via upload

---
+++ Write a program to print which is greater than or less than.py.py
@@ -0,0 +1,11 @@
+ Write a program to print which is greater than or less than
+def sample(x,y,z):
+    if x>=0 and y>=0:
+        print(x)
+    elif x<0 and z>0:
+        print(y)
+    elif y<=0 and z>=0:
+        print(y)
+    else:
+        pass
+sample(1,4,2)
