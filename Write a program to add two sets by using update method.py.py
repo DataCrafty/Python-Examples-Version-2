ccae3a8f31471e424f4a203d8f28bd668b3ac214
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-11 01:21:00 +0530

    Add files via upload

---
+++ Write a program to add two sets by using update method.py.py
@@ -0,0 +1,8 @@
+ #Write a program to add two sets by using update method
+def s(my,my_list):
+    return my+my_list
+my = {'chandu','shall','vini'}
+my_list = ['kavi','ani','pandu']
+my.update(my_list)
+print(my)
+    
