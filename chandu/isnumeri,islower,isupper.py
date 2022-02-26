9d21bb978b9372a8be5e8e05076b09797d3fcdec
Chandrakala B <99250706+DataCrafty@users.noreply.github.com>
2022-02-26 19:23:48 +0530

    Add files via upload

---
+++ chandu/isnumeri,islower,isupper.py
@@ -0,0 +1,35 @@
+def s(name):
+    l = name.isnumeric()
+    return l
+name ='23'
+s(name)
+
+def s(name):
+    l = name.isalpha()
+    return l
+name ='python 23'
+s(name)
+
+def s(name):
+    l = name.islower()
+    return l
+name ='python 23'
+s(name)
+
+def s(name):
+    l = name.isnumeric()
+    return l
+name ='23'
+s(name)
+
+def s(name):
+    l = name.isalpha()
+    return l
+name ='python 23'
+s(name)
+
+def s(name):
+    l = name.isupper()
+    return l
+name ='python 23'
+s(name)
